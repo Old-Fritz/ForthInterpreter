c2671777ee51d18ebf23f18bd07c841c228e90cf
Old-Fritz <komarov.99@yandex.ru>
2019-06-12 16:07:11 +0300

    add word add macros

---
+++ macros.asm
@@ -0,0 +1,35 @@
+; last word
+%define _lw 0
+
+%macro native 3
+	section .data
+	wh_ %+ %2 : dq _lw
+	db 0
+	db %1, 0
+	db %3
+	; update the reference to the last word
+	%define _lw wh_ %+ %2
+	xt_ %+ %2 : dq i_ %+ %2
+	; implementation starts here
+	section .text
+	i_ %+ %2:
+%endmacro
+
+%macro native 2
+	native %1, %2, 0
+%endmacro
+
+%macro colon 3
+	section .data
+	wh_ %+ %2 : dq _lw
+	db 0
+	db %1, 0
+	db %3
+	; update the reference to the last word
+	%define _lw wh_ %+ %2
+	xt_ %+ %2 : dq xt_colon
+%endmacro
+
+%macro colon 2
+	colon %1, %2, 0
+%endmacro
